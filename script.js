3c31f8b3ca53e2377c87ed198a66235b6902cf5b
Dima34 <57358582+Dima34@users.noreply.github.com>
2022-05-19 23:37:43 +0300

    mobile added

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
     
-function delayedFrameLoad() {
-    const delayedIFrames = document.querySelectorAll('iframe[data-delayedSrc]');
+    function delayedFrameLoad() {
+        const delayedIFrames = document.querySelectorAll('iframe[data-delayedSrc]');
 
-    delayedIFrames.forEach(el=>{
-        el.setAttribute("src", el.getAttribute("data-delayedSrc"));
-    })        
-}
+        delayedIFrames.forEach(el=>{
+            el.setAttribute("src", el.getAttribute("data-delayedSrc"));
+        })        
+    }
 
-    setTimeout(()=>delayedFrameLoad(), 200)
-    
+    document.addEventListener("DOMContentLoaded", ()=>{
+        setTimeout(()=>delayedFrameLoad(), 200)
+    });
